96c2f8850cc72ac690136ca1bf36a666d91844f7
Wipeshot <79708519+Wipeshot@users.noreply.github.com>
2021-03-23 11:58:57 +0100

    Changes: RegenerationSpell

--- src/me/mustache/spells/RegenerationSpell.java
+++ src/me/mustache/spells/RegenerationSpell.java
@@ -3,25 +3,41 @@ package me.mustache.spells;
 import me.mustache.character.Health;
 import me.mustache.character.Mana;
 import me.mustache.main.Main;
+import java.lang.*;
 
 public class RegenerationSpell extends Spell{
 
-    public RegenerationSpell(){ super("Regenerationszauber", "Heilt dich innerhalb einiger Sekunden um 20LP.", 15);
+    public RegenerationSpell(){ super("Regenerationszauber", "Heilt dich innerhalb 20 Sekunden um 20LP.", 15);
 
     }
 
+    @Override
     public void useSpell() {
 
         Health health = Main.thePlayer.getHealth();
         Mana mana = Main.thePlayer.getMana();
 
-        if (mana.getMana() > manaC) {
-            if (health.getHealth() < health.getMaxHealth() - 20) {
-                health.setHealth(health.getHealth() + 1);
+        int currentH = health.getHealth();
+        int maxH = health.getMaxHealth();
+        int currentM = mana.getMana();
+
+
+        for (int i = 0; i < 20; i++) {
+
+            if (currentM > manaC) {
+                if (currentH < maxH - 20) {
+                    health.setHealth(currentH + 1);
+                    mana.setMana(currentM - manaC);
+                }
             }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
-        mana.setMana(mana.getMana() - manaC);
     }
 
 }
+
+
