94d0081471397116068e4f7bc11d8457ecf1ea25
Flitzekatze <Desperado20w@hotmail.de>
2021-05-12 18:29:36 +0200

    Added Db Driver sqlite

--- src/me/mustache/main/Main.java
+++ src/me/mustache/main/Main.java
@@ -8,6 +8,11 @@ import me.mustache.gui.MetadataInventar;
 import me.mustache.consumables.HealthPot;
 import me.mustache.consumables.ManaPot;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class Main {
 
@@ -28,8 +33,25 @@ public class Main {
         MetadataInventar.getInstance().addConsumable(new ManaPot());
         MetadataInventar.getInstance().addConsumable(new Bread());
 
-
+        createNewDatabase("test.db");
 
 
     }
+    public static void createNewDatabase(String fileName) {
+
+        String url = "jdbc:sqlite:database/" + fileName;
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
 }
