ff268ffa2c29a5a96c58eb46c126ac5d0becedf5
Wipeshot <79708519+Wipeshot@users.noreply.github.com>
2021-05-12 20:05:53 +0200

    Database
    start of story

---
+++ src/me/mustache/database/Database.java
@@ -0,0 +1,81 @@
+package me.mustache.database;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+public class Database {
+
+    private static String url;
+    private List<String> answers = new List<String>();
+
+    public Database(){
+
+    }
+
+    public static void createNewDatabase(String fileName) {
+
+        setUrl("jdbc:sqlite:database/" + fileName);
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void createStory(){
+        String tableStory = "CREATE TABLE IF NOT EXISTS story (\n"
+                + " storyId INTEGER PRIMARY KEY AUTOINCREMENT,\n"
+                + " storyText TEXT,"
+                + " answerIds TEXT"
+                + ");";
+
+        String tableAnswer = "CREATE TABLE IF NOT EXISTS answer (\n"
+                + " answerId INTEGER PRIMARY KEY AUTOINCREMENT\n"
+                + " answerText TEXT NOT NULL,\n"
+                + ");";
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getFirstStory(){
+        String firstStory = "SELECT storyText \n"
+                    + " FROM story"
+                    + " WHERE storyId = 0"
+                    + ";";
+        return firstStory;
+    }
+
+    public List<String> getFirstAnswers(){
+        for(int i = 0; i < 4; i++) {
+            answers.add("SELECT answerText"
+                    + "FROM answer"
+                    + "WHERE answerId = " + i
+                    + ";");
+        }
+        return answers;
+    }
+
+    public String getStoryByAnswer(int answerID){
+        String nextStory = " SELECT storyText"
+                        + " FROM story s, answer a"
+                        + " WHERE s.answerId = " + answerID;
+        return nextStory;
+    }
+
+
+}
--- src/me/mustache/main/Main.java
+++ src/me/mustache/main/Main.java
@@ -3,6 +3,7 @@ package me.mustache.main;
 import me.mustache.character.Player;
 
 import me.mustache.consumables.Bread;
+import me.mustache.database.Database;
 import me.mustache.gui.StartGui;
 import me.mustache.gui.MetadataInventar;
 import me.mustache.consumables.HealthPot;
@@ -33,24 +34,9 @@ public class Main {
         MetadataInventar.getInstance().addConsumable(new ManaPot());
         MetadataInventar.getInstance().addConsumable(new Bread());
 
-        createNewDatabase("test.db");
+         Database.createNewDatabase("test.db");
 
 
-    }
-    public static void createNewDatabase(String fileName) {
-
-        String url = "jdbc:sqlite:database/" + fileName;
-
-        try (Connection conn = DriverManager.getConnection(url)) {
-            if (conn != null) {
-                DatabaseMetaData meta = conn.getMetaData();
-                System.out.println("The driver name is " + meta.getDriverName());
-                System.out.println("A new database has been created.");
-            }
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
     }
 
 
