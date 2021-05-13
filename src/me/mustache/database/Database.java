ff5c6ae254dd31d2e67eb4ae35ed4b7b1c97ebe6
Wipeshot <79708519+Wipeshot@users.noreply.github.com>
2021-05-13 17:11:41 +0200

    fixxed list in Database

--- src/me/mustache/database/Database.java
+++ src/me/mustache/database/Database.java
@@ -4,12 +4,182 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.List;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
 
 public class Database {
 
     private static String url;
-    private List<String> answers = new List<String>();
+    private final List<String> answers = new List<String>() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            return false;
+        }
+
+        @Override
+        public Iterator<String> iterator() {
+            return null;
+        }
+
+        @Override
+        public void forEach(Consumer<? super String> action) {
+            List.super.forEach(action);
+        }
+
+        @Override
+        public Object[] toArray() {
+            return new Object[0];
+        }
+
+        @Override
+        public <T> T[] toArray(T[] a) {
+            return null;
+        }
+
+        @Override
+        public <T> T[] toArray(IntFunction<T[]> generator) {
+            return List.super.toArray(generator);
+        }
+
+        @Override
+        public boolean add(String s) {
+            return false;
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(Collection<? extends String> c) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(int index, Collection<? extends String> c) {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public boolean removeIf(Predicate<? super String> filter) {
+            return List.super.removeIf(filter);
+        }
+
+        @Override
+        public boolean retainAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public void replaceAll(UnaryOperator<String> operator) {
+            List.super.replaceAll(operator);
+        }
+
+        @Override
+        public void sort(Comparator<? super String> c) {
+            List.super.sort(c);
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+
+        @Override
+        public String get(int index) {
+            return null;
+        }
+
+        @Override
+        public String set(int index, String element) {
+            return null;
+        }
+
+        @Override
+        public void add(int index, String element) {
+
+        }
+
+        @Override
+        public String remove(int index) {
+            return null;
+        }
+
+        @Override
+        public int indexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public int lastIndexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public ListIterator<String> listIterator() {
+            return null;
+        }
+
+        @Override
+        public ListIterator<String> listIterator(int index) {
+            return null;
+        }
+
+        @Override
+        public List<String> subList(int fromIndex, int toIndex) {
+            return null;
+        }
+
+        @Override
+        public Spliterator<String> spliterator() {
+            return List.super.spliterator();
+        }
+
+        @Override
+        public Stream<String> stream() {
+            return List.super.stream();
+        }
+
+        @Override
+        public Stream<String> parallelStream() {
+            return List.super.parallelStream();
+        }
+    };
 
     public Database(){
 
