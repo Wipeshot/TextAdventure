b0ca035ff8373d7ee6ed800d9d31ed1229c521cf
Wipeshot <79708519+Wipeshot@users.noreply.github.com>
2021-06-08 10:21:15 +0200

    useItem(potion)

--- src/me/mustache/items/Potion.java
+++ src/me/mustache/items/Potion.java
@@ -1,9 +1,13 @@
 package me.mustache.items;
 
+import me.mustache.character.Stats;
+import me.mustache.main.Main;
+
 public class Potion extends Consumable{
 
     private int type;
     private int value;
+    Stats stats = Main.thePlayer.getStats();
 
     public Potion(String name, String desc, int id, int value, int type){
         super(name, desc, id);
@@ -23,6 +27,16 @@ public class Potion extends Consumable{
 
     @Override
     public void useItem() {
-
+        if(type == 1){
+            if(stats.getMaxHp() >= stats.getCurrHp() + value){
+                stats.setCurrHp(stats.getCurrHp() + value);
+            }else{
+                stats.setCurrHp(stats.getMaxHp());
+            }
+        } else if(type == 2){
+            if(stats.getMaxMana() >= stats.getCurrMana() + value){
+                stats.setCurrMana(stats.getCurrMana() + value);
+            }
+        }
     }
 }
